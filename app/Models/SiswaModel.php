c8658f53d24d7cc6b68c9356eb81c478f4c3739a
chandrakarawaci <chandra.karawaci@gmail.com>
2022-08-23 22:00:54 +0700

    Inisialisasi Git Versi Awal

---
+++ app/Models/SiswaModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SiswaModel extends Model
+{
+    use HasFactory;
+    protected $table = 'tbl_m_siswa';
+    protected $primarykey = 'id';
+
+    protected $fillable = [
+        'NIS',
+        'NamaSiswa',
+        'Alamat'
+    ];
+}
