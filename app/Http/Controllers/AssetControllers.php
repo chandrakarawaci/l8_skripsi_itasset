19447c5e60e21f426d9bda523a918555bcaa0cc1
wafasa <wafasa@gmail.com>
2022-09-07 21:08:05 +0700

    fixing itasset

--- app/Http/Controllers/AssetControllers.php
+++ app/Http/Controllers/AssetControllers.php
@@ -53,22 +53,16 @@ class AssetControllers extends Controller
 		$this->validate($request, [
 			'file' => 'required|mimes:csv,xls,xlsx'
 		]);
-
 		// menangkap file excel
 		$file = $request->file('file');
-
 		// membuat nama file unik
 		$nama_file = rand().$file->getClientOriginalName();
-
 		// upload ke folder file_siswa di dalam folder public
 		$file->move('asset_files',$nama_file);
-
 		// import data
 		Excel::import(new AssetModel, public_path('/asset_files/'.$nama_file));
-
 		// notifikasi dengan session
 		Session::flash('Import Assert','Data Aset Berhasil diimport !');
-
 		// alihkan halaman kembali
 		return redirect('/admin/index');
     }
